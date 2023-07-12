0ee9450a964c2aa1fa35205fbacdf66214ade098
Projckt <projckt@protonmail.com>
2023-07-13 00:02:38 +0530

    gets jsonld of pizza owl

--- app-frontend/src/components/demos/demo-2/demo-2.tsx
+++ app-frontend/src/components/demos/demo-2/demo-2.tsx
@@ -6,13 +6,33 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class Demo2 {
+  componentDidLoad() {
+    this.fetch_ViewData();
+  }
+
+  async fetch_ViewData() {
+    let url: string = `http://localhost:4444`;
+    let options: any = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    await fetch(url, options)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.payload);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
   render() {
     return (
       <Host>
-         <p>Demo 2</p>
+        <p>Demo 2</p>
       </Host>
     );
   }
-
 }
--- app-frontend/src/components/views/v-home/v-home.tsx
+++ app-frontend/src/components/views/v-home/v-home.tsx
@@ -13,9 +13,12 @@ export class VHome {
         <h1>Demos</h1>
         <ul>
           <li>
-            <e-text>
-              <strong>1. Implements a force-directed graph</strong>
-            </e-text>
+            <e-text>2. Implements pizza ontology as force-directed graph</e-text>
+            <l-spacer value={0.5}></l-spacer>
+            <stencil-route-link url="/demo-2">View Demo</stencil-route-link>
+          </li>
+          <li>
+            <e-text>1. Implements a basic force-directed graph</e-text>
             <l-spacer value={0.5}></l-spacer>
             <stencil-route-link url="/demo-1">View Demo</stencil-route-link>
           </li>
