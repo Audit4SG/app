f6924a98be5c6603606c3c06b525428b0f9ea2a9
Projckt <projckt@protonmail.com>
2024-03-01 13:45:05 +0530

    fixes server

--- app-server/index.ts
+++ app-server/index.ts
@@ -3,15 +3,6 @@ import * as path from "path";
 const PORT = 3333;
 
 (async () => {
-  app.get("/*", (req, res) => {
-    let path_Joined: string = path.join(__dirname, "/www/index.html");
-    res.sendFile(path_Joined, (err) => {
-      if (err) {
-        res.status(500).send(err);
-      }
-    });
-  });
-
   app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
   });
